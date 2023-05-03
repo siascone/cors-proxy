b5fdc57ccd245842872dd9a29472e8c1a0f9a619
siascone <spencer.iascone@gmail.com>
2023-05-03 14:10:11 -0600

    setup basic cors proxy

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = requre("express");
+const cors = require("cors");
+const axios = require("axios");
+
+if (process.env.NODE_ENV === "development") {
+    require("dotenv").config();
+}
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    if (req.query.url) {
+        axios.get(req.query.url)
+            .then(resBody => res.send(resBody.data));
+    } else {
+        res.send("Hello World!");
+    }
+});
+
+app.listen(5001, () => {
+    console.log("Listening on PORT: 5001")
+})
